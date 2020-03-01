73aba20ed9479742f64274d501773e5eeb5e37ad
heosang <57525618+heosang@users.noreply.github.com>
2020-03-01 14:26:52 +0900

    Update 1966O.cpp

--- 1966O.cpp
+++ 1966O.cpp
@@ -1,77 +1,55 @@
 #include<iostream>
 #include<queue>
+#include<algorithm>
 
 using namespace std;
 
-queue <int> que[2], tempQue[2];
-int ary[10]={0, };//각 중요도 개수
-int func(int m, int mImpt) {
-	int temp;
-    int count=0;//프린트한 문서 수
-    int size;
-    size=que[1].size();
-	while (true) {
-            for(int k=0;k<size;k++){
-                if(que[1].front()>mImpt+1){
-                    que[0].pop();
-                    que[1].pop();
-                    count++;
-                }
-                else{
-                    temp=que[0].front();
-                    que[0].pop();
-                    que[0].push(temp);
-                    temp=que[1].front();
-                    que[1].pop();
-                    que[1].push(temp);
-                }
-            }
-            if(que[1].front()==mImpt+1){
-                que[0].pop();
-                que[1].pop();
-                count++;
-                ary[mImpt+1]--;
-            }
-            else if(que[1].front()==mImpt&&ary[mImpt+1]==0){
-                if(que[0].front()=m){
-                    return ++count;
-                }
-                else{
-                    que[0].pop();
-                    que[1].pop();
-                    count++;
-                    ary[mImpt]--;
-                }    
-            }
-            else{
-                temp=que[0].front();
-                que[0].pop();
-                que[0].push(temp);
-                temp=que[1].front();
-                que[1].pop();
-                que[1].push(temp);
-            }
+queue <pair<int, int>> que;
+int func(int m, int topImpt) {
+	int count = 0;//프린트한 문서 수
+	while (!que.empty()) {
+		if (que.front().second == topImpt) {
+			count++;
+			if (que.front().first == m) {
+				break;
+			}
+			que.pop();
+			topImpt = 0;
+			for (int i = 0;i < que.size();i++) {
+				topImpt = max(topImpt, que.front().second);
+				que.push(que.front());
+				que.pop();
+			}
+		}
+		else {
+			que.push(que.front());
+			que.pop();
+		}
 	}
+	return count;
 }
 int main() {
-    int testNum;//testcase 수
+	int testNum;//testcase 수
 	int n;//문서의 수
-    int m;//문서의 위치
-    int impt;//문서의 중요도
-    int mImpt;//선택한 문서의 중요도
-    int ans;
-    cin>> testNum;
-    for(int i=0;i<testNum;i++){
-        cin >> n,m;
-	    for (int j = 0;i < n;i++) {
-            que[0].push(j);
-            cin>>impt;
-            ary[impt]++;
-	    	que[1].push(impt);
-            if(j=m) mImpt=impt;
-	    }
-	    ans=func(m,mImpt);
-        cout<< ans<<endl;
-    }
+	int m;//문서의 위치
+	int impt;//문서의 중요도
+	int topImpt = 0;// 중요도 가장 높은 수
+	int temp;
+	queue <int> ans;
+	cin >> testNum;
+	while (testNum--) {
+		topImpt = 0;
+		cin >> n >> m;
+		for (int i = 0;i < n;i++) {
+			cin >> impt;
+			que.push(make_pair(i, impt));
+			topImpt = max(topImpt, impt);
+		}
+		temp = func(m, topImpt);
+		cout << temp << endl;
+		while (que.size()) {
+			que.pop();
+		}
+	}
 	return 0;
 }
