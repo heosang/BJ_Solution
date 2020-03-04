c6349912747bce238665f49b3c347313dd19b58e
heosang <57525618+heosang@users.noreply.github.com>
2020-03-04 12:09:33 +0900

    Update 2438.cpp

--- solved/2438.cpp
+++ solved/2438.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
 	cin.tie(0);
 	cin.sync_with_stdio(false);
-    int n;
-    cin >>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            cout<<"*";
-        }
-        cout<<"\n";
-    }
-    return 0;
+	int n;
+	cin >> n;
+	for (int i = 1;i <= n;i++) {
+		for (int j = 0;j < i;j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
 }
