ac1db678ccaec5f20e21ab5aa8a84814ae191167
heosang <57525618+heosang@users.noreply.github.com>
2020-03-06 15:49:35 +0900

    Create setting.cpp

---
+++ simple_Algo/setting.cpp
@@ -0,0 +1,19 @@
+#pragma warning(disable:4996)
+#include <cstdio>​
+#include <algorithm>
+using namespace std;
+void func();
+
+int n;
+int aryN[1001]={0,};
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &aryN[i]);
+	}
+	return 0;
+}
+void func(){
+    return ;
+}
