f8910f6772b99480a6aef6881c507cce9f40b802
heosang <57525618+heosang@users.noreply.github.com>
2020-03-07 15:38:15 +0900

    Create 1931.cpp

---
+++ solved/그리디/1931.cpp
@@ -0,0 +1,37 @@
+#pragma warning(disable:4996)
+
+#include <cstdio>​
+#include <vector>
+#include <algorithm>
+void func();
+vector<pair<int, int >> vecN;
+using namespace std;
+int n;
+int ans = 0;
+
+int main() {
+	int t1, t2;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &t1,&t2);
+		vecN.push_back(make_pair(t2,t1));
+	}
+	sort(vecN.begin(), vecN.end());
+	func();
+	printf("%d", ans);
+	return 0;
+}
+void func() {
+	int temp=0;
+	int tStart=vecN[0].first;
+	ans++;
+	for (int i = 1;i < n;i++) {
+		if (vecN[i].second >= tStart) {
+			tStart = vecN[i].first;
+			ans++;
+		}
+	}
+
+
+	return;
+}
