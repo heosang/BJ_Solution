3747a3a9271cace885990aea2685494a116bb31b
heosang <57525618+heosang@users.noreply.github.com>
2020-04-27 23:05:44 +0900

    Create 11066.cpp

---
+++ 11066.cpp
@@ -0,0 +1,33 @@
+#pragma warning(disable:4996)
+#include <cstdio>​
+#include <algorithm>
+
+using namespace std;
+int aryK[501]={0,};//각 장의 크기
+
+int main() {
+    int T;//testcase개수
+    int k;//소설을 구성하는 장의 수
+    int j;
+    scanf("%d",&T);
+    for(int i=0;i<T;i++){
+        int ans=0;
+        scanf("%d",&k);
+        for(j=1;j<=k;j++){
+            scanf("%d",&aryK[j]);
+        }
+        sort(aryK+1,aryK+1+k);
+        int numK=k;//합친 장의 수
+        while(numK!=1){
+            for(j=1;j<=numK>>1;j++){
+                aryK[j]=aryK[j<<1]=aryK[j<<1+1];
+            }
+            if(numK%2==1){
+                aryK[j]=aryK[numK];
+            }
+            numK=j;
+        }
+        printf("%d",aryK[1]);
+    }
+	return 0;
+}
