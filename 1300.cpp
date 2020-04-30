ca1dae8f65c91eed789a0d1009d5391c3d3a2d17
heosang <57525618+heosang@users.noreply.github.com>
2020-04-30 23:52:59 +0900

    Create 1300.cpp

---
+++ 1300.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(0);
+	cin.sync_with_stdio(false);
+	int n,k;
+	cin >> n;
+    cin>>k;
+    int start=1;
+    int last=n*n;
+    int mid=(start+last)/2;
+	return 0;
+}
