0fe824600b0b77081f55b3e758d5e612fd98e1f9
heosang <57525618+heosang@users.noreply.github.com>
2020-05-03 23:47:06 +0900

    Update 2604.cpp

--- solved/DFS와BFS/2604.cpp
+++ solved/DFS와BFS/2604.cpp
@@ -1,41 +1,34 @@
 #include <iostream>
-#include <vector>
-#include<utility>
-#include <algorithm>
-int cnt=0;
+#include <stack>
+int cnt=0,comNum;
 using namespace std;
-vector <pair<int, int>> ans;
-bool ary[101] = { 0, };
+stack<int>stk;
+bool com[101][101];
+bool visit[101];
+void func(int);
 int main() {
-	int comNum, pairNum;
+	int pairNum;
 	int comA, comB;
 	cin >> comNum;
 	cin >> pairNum;
 	for (int i = 0;i < pairNum;i++) {
 		cin >> comA >> comB;
-		if (comA < comB) {
-			ans.push_back(make_pair(comA, comB));
-		}
-		else {
-			ans.push_back(make_pair(comB, comA));
-		}
+		com[comA][comB] = 1;
+		com[comB][comA] = 1;
 	}
-	sort(ans.begin(), ans.end());
-	ary[1] = true;
-	if (ans[0].first != 1) {
-		cout << "0";
-		return 0;
-	}
-	else {
-		ary[ans[0].second] = true;
-		cnt++;
-	}
-	for (int i = 1;i < pairNum;i++) {
-		if (ary[ans[i].first] == true&& ary[ans[i].second] == false) {
-			cnt++;
-			ary[ans[i].second] == true;
-		}
-	}
+	func(1);
 	cout << cnt;
 	return 0;
+}
+void func(int n) {
+	stk.push(n);
+	visit[n] = 1;
+	for (int i = 1;i <= comNum;i++) {
+		if (com[n][i] == 1 && !visit[i]) {
+			cnt++;
+			func(i);
+		}
+	}
+	stk.pop();
+	return;
 }
